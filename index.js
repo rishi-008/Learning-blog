f005f9619e5ee8e3edbc7b331d4b8990524efcbb
rishi-008 <67348997+rishi-008@users.noreply.github.com>
2024-05-23 10:34:52 -0400

    connected js file to html file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const loadMorePostsBtn = document.querySelector('#load-more-posts');
+const loadMorePostsBtn = document.querySelector('#load-more-blogs-btn');
 
 
 loadMorePostsBtn.addEventListener('click', () => {
+    console.log("Button clicked");
     for (let blogNum = 4; blogNum < 8; blogNum++) {
-        document.querySelector(`.small-img-blog-holder-${blogNum}`).classList.toggle("makeBlogVisible");
+        document.querySelector(`.small-img-blog-holder-${blogNum}`).classList.toggle('makeBlogVisible');
     }
 });
