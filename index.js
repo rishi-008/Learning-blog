52674eff6f54ec72b35c0184c40aa0ca6945ae77
rishi-008 <67348997+rishi-008@users.noreply.github.com>
2024-05-24 08:16:08 -0400

    view more blogs functionality working as expected

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const loadMorePostsBtn = document.querySelector('#load-more-blogs-btn');
 loadMorePostsBtn.addEventListener('click', () => {
     console.log("Button clicked");
     for (let blogNum = 4; blogNum < 8; blogNum++) {
-        document.querySelector(`.small-img-blog-holder-${blogNum}`).classList.toggle('makeBlogVisible');
+        const blogHolder = document.querySelector(`.small-img-blog-holder-${blogNum}`);
+        if (blogHolder) {
+            console.log("Blog holder found");
+            blogHolder.classList.toggle('makeElementVisible');
+        }
     }
 });
